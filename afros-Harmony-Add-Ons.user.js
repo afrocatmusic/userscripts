96da69eb03026056d04d3f871afd47c6602cde72
afro <159882681+afrocatmusic@users.noreply.github.com>
2025-08-14 22:16:20 -0500

    Update afros-Harmony-Add-Ons.user.js
    Another pretty big update!
    Everything works pretty much the same, but I've re written a lot of the code, especially the code to copy the list of countries.
    
    Most noticeable changes are that you can now copy the list of available countries, and the catalog number as well.
    
    changelog:
    - bumped to version 1.2
    - added custom css classes instead of relying on inline styling
    - replaced all instances of `var` with either `let` or `const` 
    - rewrote the qobuz code, now uses an object instead of a map to get the relevant region. also reduced the entire `if ... else` logic to pretty much a single line using `.map` and `.find` 
    - replaced the constructed search URLs with template literals
    - added some comments that should make future maintenance easier
    - realized I don't need to add two different event listeners to elements, so moved the notification triggers and the copying to clipboard to the same event listener
    - moved all of the `.mouseover / .mouseout` functions that were just changing text color to a simple `:hover` css
    - Rewrote the entire `copyCountries()` function. Now instead of doing everything in a single function, it's split into a few different functions handling different tasks. This happened because I realized what a mess this would be to enable copying the "Availability" list.
    A lot of the code was cut down because I also realized Harmony assigns the class `.region-list` to the lists, so this cuts down on having to find them by iterating through the table to find the one cell with the "unavailability" text and so on.
    Also found out I can get the lists by checking the `abbr` tags on the flags when the lists are collapsed. It's much easier to get the lists on page load and not have to worry about the lists being expanded or not. 
    - added the ability to copy the catalog number in a single click as well. The blue background and dotted underline should make it obvious that it's copyable
    - overall reduced the code by about a hundred lines(!!) or about 5k characters
    
    In the next update I'll probably change the permalink href `javascript:void(0)` to something else, maybe just a clickable button instead of a link. See https://stackoverflow.com/a/1293130

--- afros-Harmony-Add-Ons.user.js
+++ afros-Harmony-Add-Ons.user.js
@@ -4,7 +4,7 @@
 // @updateURL   https://raw.github.com/afrocatmusic/userscripts/main/afros-Harmony-Add-Ons.user.js
 // @downloadURL https://raw.github.com/afrocatmusic/userscripts/main/afros-Harmony-Add-Ons.user.js
 // @match       https://harmony.pulsewidth.org.uk/release?*
-// @version     1.1
+// @version     1.2
 // @author      afro
 // @grant       GM_setClipboard
 // @grant       GM.setClipboard
@@ -12,7 +12,29 @@
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @require     https://raw.githubusercontent.com/jpillora/notifyjs/refs/heads/master/dist/notify.js
 // ==/UserScript==
-
+function css() {
+  let head = document.getElementsByTagName('head')[0];
+  if (head) {
+    let style = document.createElement('style');
+    style.setAttribute('type', 'text/css');
+    style.textContent = `
+    .copyable-background {
+      background: rgba(22, 45, 171, 0.3);
+      padding: 0.5em;
+      }
+    .copyable-text {
+      color: white;
+      text-decoration: underline dotted;
+      cursor: pointer;
+    }
+    .copyable-text:hover {
+      color: #add8e6;
+    }
+      `;
+    head.appendChild(style);
+    }
+}
+css();
 async function writeClipboardText(text) {
     try {
       await navigator.clipboard.writeText(text);
@@ -24,215 +46,226 @@ function showCopyNotif() {
   $.notify('Copied!',{autoHideDelay:'1500', className:'success', position:'bottom'});
 }
 
+//--- add search links for youtube music, qobuz, and beatsource
 function addSearchLinks(){
-  var separator = document.createTextNode(' | ');
-//deal with VA
+  let separator = document.createTextNode(' | ');
+  //deal with VA
   let relArtist = '';
   if ($('.artist-credit')[0].childElementCount < 5) {
         relArtist = $('.release-artist')[0].textContent.slice(3).replaceAll('&','%26').replaceAll('/',' ');
     } else {
         relArtist = 'Various Artists';
     }
-  var relTitle = $('.release-title')[0].textContent.replaceAll('&','%26').replaceAll('/',' ');
-//YTM barcode lookup
-  var allHeaders = $('th');
-  var headerNames = [];
-  for (var i = 0; i < allHeaders.length; i++) {
+  let relTitle = $('.release-title')[0].textContent.replaceAll('&','%26').replaceAll('/',' ');
+  //YTM barcode lookup
+  let allHeaders = $('th');
+  let headerNames = [];
+  for (let i = 0; i < allHeaders.length; i++) {
     headerNames.push(allHeaders[i].innerText);
   }
-  var gtinIndex = headerNames.indexOf('GTIN');
-  var barcodeArea = $('th')[gtinIndex];
-  var barcode = barcodeArea.nextElementSibling.textContent.replace(/^0+/,''); //remove leading zeroes
-  var ytRelURL = 'https://music.youtube.com/search?q=%22'+barcode+'%22';
-  var ytReleaseLink = document.createTextNode('Search YouTube Music');
-  var ytRelAnchor = document.createElement('a');
+  let gtinIndex = headerNames.indexOf('GTIN');
+  let barcodeArea = $('th')[gtinIndex];
+  let barcode = barcodeArea.nextElementSibling.textContent.replace(/^0+/,''); //remove leading zeroes
+  let ytRelURL = `https://music.youtube.com/search?q="${barcode}"`;
+  let ytReleaseLink = document.createTextNode('Search YouTube Music');
+  let ytRelAnchor = document.createElement('a');
       ytRelAnchor.appendChild(ytReleaseLink);
       ytRelAnchor.setAttribute('href',ytRelURL);
-//qobuz
-  var qbzRegion;
+  //qobuz
   let defaultQbzRegion = 'us-en'; //feel free to change
-  var region = $('#region-input')[0].value.toLowerCase();
-  var regionMap = new Map([
-    ['ar','ar-es'],
-    ['au','au-en'],
-    ['at','at-de'],
-    ['be','be-nl'],
-    ['br','br-pt'],
-    ['ca','ca-en'],
-    ['cl','cl-es'],
-    ['co','co-es'],
-    ['dk','dk-en'],
-    ['fi','fi-en'],
-    ['fr','fr-fr'],
-    ['de','de-de'],
-    ['ie','ie-en'],
-    ['it','it-it'],
-    ['jp','jp-ja'],
-    ['lu','lu-de'],
-    ['mx','mx-es'],
-    ['nl','nl-nl'],
-    ['nz','nz-en'],
-    ['no','no-en'],
-    ['pt','pt-pt'],
-    ['es','es-es'],
-    ['se','se-en'],
-    ['ch','ch-de'],
-    ['gb','gb-en'],
-    ['us','us-en'],
-  ]);
-  if (region.includes(',')) { //if multiple regions, use the first one that exists in regionMap
-    let regionTest = region.split(',');
-    for (let i = 0; i < regionTest.length; i++) {
-      if (regionMap.has(regionTest[i])) {
-        qbzRegion = regionMap.get(regionTest[i]);
-        break;
-      }
-    }
-      if (qbzRegion === undefined) { //if it's all unsuported regions
-        qbzRegion = defaultQbzRegion;
-      }
-  }
-  else if (regionMap.has(region) === true) { //if it's only one supported region
-    qbzRegion = regionMap.get(region);
-  }
-  else if (regionMap.has(region) === false) { //if it's one unsupported region
-    qbzRegion = defaultQbzRegion;
-  }
-  var qbzSearchURL = 'https://www.qobuz.com/'+qbzRegion+'/search/albums/'+relArtist+'%20'+relTitle;
-  var linkQbzText = document.createTextNode('Search Qobuz');
-  var qbzAnchor = document.createElement('a');
+  let regionInput = $('#region-input').val().toLowerCase();
+  const regionMap = {
+    ar: 'ar-es', au: 'au-en', at: 'at-de', be: 'be-nl', br: 'br-pt',
+    ca: 'ca-en', cl: 'cl-es', co: 'co-es', dk: 'dk-en', fi: 'fi-en',
+    fr: 'fr-fr', de: 'de-de', ie: 'ie-en', it: 'it-it', jp: 'jp-ja',
+    lu: 'lu-de', mx: 'mx-es', nl: 'nl-nl', nz: 'nz-en', no: 'no-en',
+    pt: 'pt-pt', es: 'es-es', se: 'se-en', ch: 'ch-de', gb: 'gb-en',
+    us: 'us-en'
+  };
+  let regionKey = regionInput.split(',').map(code => code.trim()).find(code => regionMap[code]);
+  let qbzRegion = regionMap[regionKey] || defaultQbzRegion;
+
+  let qbzSearchURL = `https://www.qobuz.com/${qbzRegion}/search/albums/${relArtist} ${relTitle}`;
+  let linkQbzText = document.createTextNode('Search Qobuz');
+  let qbzAnchor = document.createElement('a');
       qbzAnchor.appendChild(linkQbzText);
       qbzAnchor.setAttribute('href',qbzSearchURL);
-//beatsource
-  var btsSearchURL = 'https://www.beatsource.com/search/releases?q='+relArtist+'%20'+relTitle;
-  var linkBtsText = document.createTextNode('Search Beatsource');
-  var btsAnchor = document.createElement('a');
+  //beatsource
+  let btsSearchURL = `https://www.beatsource.com/search/releases?q=${relArtist} ${relTitle}`;
+  let linkBtsText = document.createTextNode('Search Beatsource');
+  let btsAnchor = document.createElement('a');
       btsAnchor.appendChild(linkBtsText);
-      btsAnchor.setAttribute('href',btsSearchURL);
-//placing
-  var space = $('h2.center')[0];
+      btsAnchor.setAttribute('href', btsSearchURL);
+  //placing
+  let space = $('h2.center')[0];
       space.nextElementSibling.append(qbzAnchor,separator,ytRelAnchor,separator.cloneNode(),btsAnchor);
 }
+addSearchLinks();
 
+//--- copy external links
 function copyLinks(){
-  var allHeaders = Array.from($('th'));
-  var headerNames = [];
-  for (var i = 0; i < allHeaders.length; i++) {
+  let allHeaders = $('th');
+  let headerNames = [];
+  for (let i = 0; i < allHeaders.length; i++) {
      headerNames.push(allHeaders[i].innerHTML);
   }
-  var extLinksIndex = headerNames.indexOf('External links'); //position of external links text in the table
-  var exLiArea = $('th')[extLinksIndex];
-      exLiArea.setAttribute('style','background: rgba(22, 45, 171, 0.3)');
-  var links = Array.from(exLiArea.nextElementSibling.querySelectorAll('a')).toString().split(',').join('\n');
+  let extLinksIndex = headerNames.indexOf('External links'); //position of external links text in the table
+  let exLiArea = $('th')[extLinksIndex];
+      exLiArea.setAttribute('class','copyable-background');
+  let links = Array.from(exLiArea.nextElementSibling.querySelectorAll('a')).toString().split(',').join('\n');
 
-  var extAnchor = document.createElement('a');
+  let extAnchor = document.createElement('a');
       extAnchor.textContent = 'External links';
       extAnchor.title = 'Click to copy external links';
-      extAnchor.addEventListener("click", () => writeClipboardText(links));
+      extAnchor.addEventListener("click", () => {
+        writeClipboardText(links);
+        showCopyNotif();
+      });
       exLiArea.innerText = '';
       exLiArea.appendChild(extAnchor);
-      extAnchor.setAttribute('style', 'cursor: pointer; text-decoration: underline dotted; color: white;');
-      extAnchor.addEventListener("click", () => showCopyNotif());
-      extAnchor.onmouseover = function() {mouseOver();};
-      extAnchor.onmouseout = function() {mouseOut();};
-        function mouseOver() {
-          extAnchor.style.color = "#add8e6";
-        }
-
-        function mouseOut() {
-          extAnchor.style.color = "white";
-        }
+      extAnchor.setAttribute('class','copyable-text');
 }
+copyLinks();
 
+//--- copy GTIN / barcode
 function copyBarcode(){
-  var allHeaders = Array.from($('th'));
-  var headerNames = [];
-  for (var i = 0; i < allHeaders.length; i++) {
+  let allHeaders = Array.from($('th'));
+  let headerNames = [];
+  for (let i = 0; i < allHeaders.length; i++) {
      headerNames.push(allHeaders[i].innerText);
   }
-  var gtinIndex = headerNames.indexOf('GTIN');
-  var barcodeArea = $('th')[gtinIndex];
-  var barcode = barcodeArea.nextElementSibling.textContent;
-  var barcodeAnchor = document.createElement('a');
+  let gtinIndex = headerNames.indexOf('GTIN');
+  let barcodeArea = $('th')[gtinIndex];
+  let barcode = barcodeArea.nextElementSibling.textContent;
+  let barcodeAnchor = document.createElement('a');
       barcodeAnchor.textContent = 'GTIN';
       barcodeAnchor.title = 'Click to copy';
-  var copyableAreas = [barcodeArea, barcodeArea.nextElementSibling]; //make the whole area clickable
+  let copyableAreas = [barcodeArea, barcodeArea.nextElementSibling]; //make the whole area clickable
       copyableAreas.forEach(function(elem){
-        elem.addEventListener("click", () => writeClipboardText(barcode));
-        elem.addEventListener("click", () => showCopyNotif());
+        elem.addEventListener("click", () => {
+            writeClipboardText(barcode);
+            showCopyNotif();
         });
+      });
       barcodeArea.innerText = '';
       barcodeArea.appendChild(barcodeAnchor);
-      barcodeAnchor.setAttribute('style', 'cursor: pointer; text-decoration: underline dotted; color: white;');
-      barcodeAnchor.onmouseover = function() {mouseOver();};
-      barcodeAnchor.onmouseout = function() {mouseOut();};
-        function mouseOver() {
-          barcodeAnchor.style.color = "#add8e6";
-          }
-        function mouseOut() {
-          barcodeAnchor.style.color = "white";
-          }
-      barcodeArea.setAttribute('style','background: rgba(22, 45, 171, 0.3); padding: 1em; cursor: pointer;');
-      barcodeArea.nextElementSibling.setAttribute('style','background: rgba(22, 45, 171, 0.3); cursor: pointer;');
+      barcodeAnchor.setAttribute('class', 'copyable-text');
+      barcodeArea.setAttribute('class','copyable-background');
+      barcodeArea.nextElementSibling.setAttribute('class','copyable-background');
+}
+copyBarcode();
+
+//--- Copy country lists
+const areas = $('.region-list');
+let availableCountries;
+let unavailableCountries;
+
+function checkAv_Un() {
+  if (areas.length === 0) { //happens with apple music, tidal, and beatport
+    return {areas: [], availableArea: null, unavailableArea: null}; //return empty object
+  }
+
+  let sections = [];
+  areas.each(function(_, entry) { //ignore index
+    sections.push($(entry).parent().parent().children().first().text());
+  });
+
+  let availableArea = null, unavailableArea = null;
+  if (sections.length === 2) { //don't know if theres cases where one list appears and the other one doesn't
+    availableArea = areas[0];
+    unavailableArea = areas[1];
+  }
+
+  return {areas, availableArea, unavailableArea};
 }
 
-function copyCountries() {
-  if ($('.release-info')[0].innerText.search('Unavailability') === -1) {
-    return;
-  } else {
-      var allHeaders = Array.from($('th'));
-      var headerNames = [];
-      for (var i = 0; i < allHeaders.length; i++) {
-         headerNames.push(allHeaders[i].innerText);
-      }
-      var today = new Date().toISOString().slice(0, 10);
-      var unavailIndex = headerNames.indexOf('Unavailability');
-      var unavailArea = $('th')[unavailIndex];
-          unavailArea.setAttribute('style','background: rgba(22, 45, 171, 0.3); padding: 1em;');
-      var expandButton = unavailArea.nextElementSibling.querySelector('button');
-          if (expandButton === null) {return;}
-          else {
-            expandButton.click(); //make sure list is expanded by default
-          }
-      var unavailableCountries;
-      var unavailAnchor = document.createElement('a');
-          unavailAnchor.textContent = 'Unavailability';
-          unavailAnchor.setAttribute('style','text-decoration: underline dotted;');
-          unavailAnchor.title = 'Click to copy';
-          unavailAnchor.addEventListener("click", () => writeClipboardText(unavailableCountries));
-          unavailArea.innerText = '';
-          unavailArea.appendChild(unavailAnchor);
-          unavailAnchor.setAttribute('style', 'cursor: pointer; text-decoration: underline dotted; color: white;');
-          unavailAnchor.addEventListener("click", () => showCopyNotif());
-          unavailAnchor.onmouseover = function() {mouseOver();};
-          unavailAnchor.onmouseout = function() {mouseOut();};
-            function mouseOver() {
-              unavailAnchor.style.color = "#add8e6";
-              if (unavailArea.nextElementSibling.innerText.split('\n').length > 30) {
-                unavailableCountries = 'Unavailable in these regions, as of '+today+':\n' + unavailArea.nextElementSibling.innerText
-                  .replace(/(.*regionsCollapse)/gm,'')
-                  .replace(/^(.*)\n(.*)\n(.*)/gm,'$1 $2 $3')
-                  .replace(/ $/gm,'');
-                } else {
-                  unavailableCountries = 'Unavailable in these regions, as of '+today+':\n' + unavailArea.nextElementSibling.innerText.replace(/(\n.*regionsCollapse)/gm,'');
-                }
-            }
-            function mouseOut() {
-              unavailAnchor.style.color = "white";
-              }
+function extract(tags) {
+  let regionArray = [];
+  tags.each(function(_, region) { //ignore index
+    regionArray.push(region.textContent + ' ' + region.title);
+  });
+  let list = regionArray.join('\n');
+  if (regionArray.length > 30) {
+    list = (list.trimEnd() + '\n') //add a newline at the end so the regex works with the last country
+      .replace(/^(.*)\n(.*)\n(.*)/gm, '$1 | $2 | $3') //split into three columns and add | delimiters
+      .replace(/( \| )$|(\n)$/gm, ''); //remove last delimiter or newline
   }
+  return list;
 }
+
+function getLists() {
+  let result = checkAv_Un();
+  if (!result.availableArea || !result.unavailableArea) {return;}
+
+  let a_abbrTags = $(result.availableArea).find('abbr');
+  let u_abbrTags = $(result.unavailableArea).find('abbr');
+
+  availableCountries = extract(a_abbrTags);
+  unavailableCountries = extract(u_abbrTags);
+}
+getLists(); //run on page load
+
+function placeOnPage() {
+  let today = new Date().toISOString().slice(0, 10);
+  let checkResult = checkAv_Un();
+  if (checkResult.areas.length === 0) {return;}
+
+  let avAreaHeader = $(checkResult.availableArea).parent().parent().children().first()[0];
+  let unavAreaHeader = $(checkResult.unavailableArea).parent().parent().children().first()[0];
+
+  avAreaHeader.setAttribute('class','copyable-background');
+  unavAreaHeader.setAttribute('class','copyable-background');
+
+  let avAnchor = document.createElement('a');
+    avAnchor.textContent = 'Availability';
+    avAnchor.setAttribute('class', 'copyable-text');
+    avAnchor.title = 'Click to copy';
+    avAnchor.addEventListener('click', () => {
+      writeClipboardText(`=== Available in the following regions, as of ${today} ===\n${availableCountries}`);
+      showCopyNotif();
+    });
+    avAreaHeader.textContent = '';
+    avAreaHeader.appendChild(avAnchor);
+
+  let unavAnchor = document.createElement('a');
+    unavAnchor.textContent = 'Unavailability';
+    unavAnchor.setAttribute('class', 'copyable-text');
+    unavAnchor.title = 'Click to copy';
+    unavAnchor.addEventListener('click', () => {
+      writeClipboardText(`=== Unavailable in the following regions, as of ${today} ===\n${unavailableCountries}`);
+      showCopyNotif();
+    });
+    unavAreaHeader.textContent = '';
+    unavAreaHeader.appendChild(unavAnchor);
+}
+placeOnPage(); //run on page load
+
+//--- make permalink copyable in one click
 function copyPermalink() {
-  var permaLink = $('p.center > a')[0];
-  var permaLinkURL = permaLink.href;
+  let permaLink = $('p.center > a')[0];
+  let permaLinkURL = permaLink.href;
       permaLink.href = "javascript:void(0);";
       permaLink.addEventListener("click", () => writeClipboardText(permaLinkURL));
 }
+copyPermalink();
 
-window.setTimeout(function() {
-  addSearchLinks();
-  copyLinks();
-  copyBarcode();
-  copyCountries();
-  copyPermalink();
-}, 60);
+//--- make catalog number copyable in one click
+function copyCatNo() {
+  if ($('.release-labels')[0].lastChild.lastChild) { //check if there's a catalog number
+    let catNoElement = $('.release-labels')[0].lastChild.lastChild;
+    let catNo = catNoElement.textContent.trim();
+    let catNoAnchor = document.createElement('a');
+    catNoAnchor.textContent = catNoElement.textContent.trim();
+    catNoAnchor.title = 'Click to copy: '+catNo;
+    catNoElement.textContent = '';
+    catNoElement.parentElement.append(catNoAnchor);
+    catNoAnchor.setAttribute('class','copyable-text');
+    catNoAnchor.style.padding = '10px';
+    catNoAnchor.style.marginLeft = '1em';
+    catNoAnchor.addEventListener('click',() =>{
+      writeClipboardText(catNo);
+      showCopyNotif();
+    });
+  }
+}
+copyCatNo();
