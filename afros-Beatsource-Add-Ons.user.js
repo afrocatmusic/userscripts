f65a68c3a9d641522cf57778a90498c336610ddd
afro <159882681+afrocatmusic@users.noreply.github.com>
2025-11-13 16:13:08 -0600

    Handle SPA navigation
    Pretty much entirely new code, it took me a while to figure out how to deal with beatsource's navigation shenanigans
    The script will now automatically get the right data when navigating to another release. It no longer needs to make every <a> element reload the page (what the previous linkMe() function did)
    
    Other than the navigation fixes, not much has changed. The harmony link now looks like a link instead of plain text. I also made it so that if the catalog number is exactly the same as the barcode, it'll just skip showing it at all, since it's likely not a real catalog number

--- afros-Beatsource-Add-Ons.user.js
+++ afros-Beatsource-Add-Ons.user.js
@@ -1,65 +1,196 @@
 // ==UserScript==
-// @name        afro's Beatsource Add-Ons
-// @namespace   https://github.com/afrocatmusic/userscripts
-// @updateURL   https://raw.github.com/afrocatmusic/userscripts/main/afros-Beatsource-Add-Ons.user.js
-// @match       https://www.beatsource.com/*
-// @grant       none
-// @version     1.0
-// @author      afro
-// @description Adds some extra info to beatsource release pages + handy links for importing into MusicBrainz
-// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js
+// @name          afro's Beatsource Add-Ons
+// @namespace     https://github.com/afrocatmusic/userscripts
+// @updateURL     https://raw.github.com/afrocatmusic/userscripts/main/afros-Beatsource-Add-Ons.user.js
+// @downloadURL   https://raw.github.com/afrocatmusic/userscripts/main/afros-Beatsource-Add-Ons.user.js
+// @match         https://www.beatsource.com/*
+// @grant         GM.xmlHttpRequest
+// @grant         GM.getValue
+// @grant         GM.setValue
+// @version       1.1
+// @author        afro
+// @description   Adds some extra info to beatsource release pages + handy links for importing into MusicBrainz
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js
 // ==/UserScript==
 
-//Important, forces reload when clicking a link
-//Needs to happen for the info to update
-function linkMe(){
-  var links = $('a');
-      for (var i = links.length - 1; i >= 0; i--) {
-      links[i].setAttribute('target','_top')
-      };
+function parseDataFromHTML(htmlContent) {
+  const tempDiv = document.createElement('div');
+  tempDiv.innerHTML = htmlContent;
+
+  const nextDataElement = tempDiv.querySelector('#__NEXT_DATA__');
+
+  if (!nextDataElement) {
+    return null;
   }
-function findStuff(){
-  if (window.location.href.search(/\/release\//) > -1) {
-    var area = $('[data-testid="col-sidebar"]')[0];
-    var rawReleaseInfo = JSON.parse(document.getElementById('__NEXT_DATA__').innerText).props.pageProps;
-    var barcode = rawReleaseInfo.release.upc;
-    var catNum = rawReleaseInfo.release.catalog_number;
-    var isrc = rawReleaseInfo.tracks.results[0].isrc;
-    var relTitle = rawReleaseInfo.release.name;
-    var isrcList = rawReleaseInfo.tracks.results.map(track => track.isrc);
 
-    var infoContainer = document.createElement('div');
-        infoContainer.setAttribute('id','infoContainer');
-        infoContainer.setAttribute('style','font-size: 1.5rem; color: #415974; line-height: 2.5rem; padding-bottom: 1.5rem; margin-top: 1.5rem; white-space: pre-line');
-        area.appendChild(infoContainer);
-    var newline = document.createTextNode('\n');
+  try {
+    const rawReleaseInfo = JSON.parse(nextDataElement.textContent || nextDataElement.innerHTML).props.pageProps;
 
-    var relTitleDisplay = document.createTextNode('Title: '+relTitle);
-    var barcodeDisplay = document.createTextNode('Barcode: '+barcode);
-    var barcodeAnchor = document.createElement('a');
-        barcodeAnchor.appendChild(barcodeDisplay);
-        barcodeAnchor.setAttribute('href','https://harmony.pulsewidth.org.uk/release?category=preferred&gtin='+barcode);
-        barcodeAnchor.setAttribute('style','color: #415974');
-    var catDisplay = document.createTextNode('Catalog Number: '+catNum);
-    var isrcDisplay = document.createTextNode('ISRC: '+isrc);
+    if (rawReleaseInfo.release && rawReleaseInfo.release.name) {
+      const relTitle = rawReleaseInfo.release.name;
+      const barcode = rawReleaseInfo.release.upc;
+      const catNum = rawReleaseInfo.release.catalog_number;
+      const isrcList = rawReleaseInfo.tracks ? rawReleaseInfo.tracks.results.map(track => track.isrc) : [];
 
-//thanks to https://github.com/Onteia for helping with this bit
-    var magicISRCURL = "https://magicisrc.kepstin.ca/?";
-        for(let i = 1; i < isrcList.length+1; i++) {
-            magicISRCURL = magicISRCURL + "isrc" + i + "=" + isrcList[i-1] + "&";
+      return { barcode, relTitle, catNum, isrcList };
+    }
+  } catch (e) {
+  }
+  return null;
+}
+
+async function fetchAndGetData() {
+  //fetch the new html
+  try {
+    const url = window.location.href;
+
+    const htmlContent = await new Promise((resolve, reject) => {
+      $.get(url, function (data) {
+        if (typeof data === 'string') {
+          resolve(data);
+        } else if (data && data.documentElement) {
+          resolve(data.documentElement.outerHTML);
+        } else {
+          reject(new Error('Failed to retrieve valid HTML content.'));
         }
-        magicISRCURL = magicISRCURL.slice(0,-1);
-        magicISRCURL = magicISRCURL+'&edit-note=Import+ISRCs+from+'+window.location.href+'+using+https://github.com/afrocatmusic/userscripts/blob/main/afros-Beatsource-Add-Ons.user.js';
-    var magicISRCDisplay = document.createTextNode('Submit ISRCs');
-    var magicISRCAnchor = document.createElement('a');
-        magicISRCAnchor.appendChild(magicISRCDisplay);
-        magicISRCAnchor.setAttribute('href',magicISRCURL);
-        magicISRCAnchor.setAttribute('style','color: #415974');
+      }).fail((jqXHR, textStatus, errorThrown) => {
+        reject(new Error(`AJAX request failed: ${textStatus} ${errorThrown}`));
+      });
+    });
 
-        infoContainer.append(relTitleDisplay, newline, barcodeAnchor, newline.cloneNode(),catDisplay,newline.cloneNode(),magicISRCAnchor);
+    return parseDataFromHTML(htmlContent);
+
+  } catch (error) {
+    return null;
   }
 }
-window.setTimeout(function(){
-  linkMe();
-  findStuff();
-}, 50);
+
+function addToPage(info) {
+  $('#beatsourceAddOnsInfo').remove();
+
+  if (!info) {return;}
+
+  const area = $(sidebarTargetSelector);
+
+  if (area.length === 0) {
+    return;
+  }
+
+  const isrcParams = info.isrcList.map((isrc, index) => {
+    return `isrc${index + 1}=${isrc}`;
+  });
+
+  let magicISRCURL = `https://magicisrc.kepstin.ca/?${isrcParams.join('&')}`;
+
+  const rawEditNote = `Import ISRCs from ${window.location.href} using https://github.com/afrocatmusic/userscripts/blob/main/afros-Beatsource-Add-Ons.user.js`;
+  const editNote = encodeURIComponent(rawEditNote);
+  magicISRCURL = `${magicISRCURL}&edit-note=${editNote}`;
+
+  const injectedDiv = $(`
+    <div id="beatsourceAddOnsInfo">
+      <p>Title: ${info.relTitle}</p>
+      <p>Barcode: <a href="https://harmony.pulsewidth.org.uk/release?category=preferred&gtin=${info.barcode}">${info.barcode}</a></p>
+      <p class="addonCatNo">Catalog Number: ${info.catNum}</p>
+      <a href="${magicISRCURL}">Submit ISRCs to MusicBrainz</a>
+    </div>
+  `).css({
+    'font-size': '1.5rem',
+    'color': '#415974',
+    'line-height': '2.5rem',
+    'padding-bottom': '1.5rem',
+    'margin-top': '1.5rem',
+  });
+
+  area.append(injectedDiv);
+
+  if (info.barcode === info.catNum) {
+    injectedDiv.find('.addonCatNo').remove();
+  }
+}
+
+function showLoadingState() {
+  $('#beatsourceAddOnsInfo').remove();
+
+  const area = $(sidebarTargetSelector);
+  if (area.length === 0) {
+    return;
+  }
+
+  const loadingDiv = $(`
+    <div id="beatsourceAddOnsInfo">
+      <p>Loading release data...</p>
+    </div>
+  `).css({
+    'font-size': '1.5rem',
+    'color': '#415974',
+    'line-height': '2.5rem',
+    'padding-bottom': '1.5rem',
+    'margin-top': '1.5rem',
+  });
+
+  area.append(loadingDiv);
+}
+
+let lastKnownRelTitle = null;
+
+async function processPageUpdate() {
+
+  if (window._bs_addon_processing) {return;}
+  window._bs_addon_processing = true;
+
+  const info = await fetchAndGetData();
+
+  window._bs_addon_processing = false;
+
+  if (!info || !info.relTitle) {return;}
+
+  if (info.relTitle === lastKnownRelTitle) {return;}
+
+  showLoadingState();
+  await new Promise(resolve => setTimeout(resolve, 300));
+
+  addToPage(info);
+  lastKnownRelTitle = info.relTitle;
+}
+
+const originalPushState = history.pushState;
+const observerTargetSelector = '#__next';
+const sidebarTargetSelector = '[data-testid="col-sidebar"]';
+
+function setupObservers() {
+  const targetNode = $(observerTargetSelector)[0];
+  const config = { childList: true, subtree: true, characterData: true };
+
+  const callback = function (mutationsList, observer) {
+    clearTimeout(window._bs_addon_timer);
+    window._bs_addon_timer = setTimeout(processPageUpdate, 200);
+  };
+
+  const observer = new MutationObserver(callback);
+
+  if (targetNode) {
+    observer.observe(targetNode, config);
+  } else {
+    console.error("Could not find main container to observe.");
+  }
+
+  //overwrite pushState
+  history.pushState = function () {
+    const go = originalPushState.apply(this, arguments);
+    $('#beatsourceAddOnsInfo').remove();
+    lastKnownRelTitle = null;
+    setTimeout(processPageUpdate, 50);
+    return go;
+  };
+}
+
+async function run() {
+  const initialData = await fetchAndGetData();
+  if (initialData) {
+    lastKnownRelTitle = initialData.relTitle;
+    addToPage(initialData);
+  }
+
+  setupObservers();
+}
+run();
